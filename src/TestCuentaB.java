9f1b8efecd17f96e8c25287c79093f44b33c7266
Mario vg <22670080@itiguala.edu.mx>
2023-03-14 19:59:17 -0600

    se creo la "clase SportShoe (examen)"

--- src/TestCuentaB.java
+++ src/TestCuentaB.java
@@ -1,56 +1,82 @@
+import javax.sound.midi.Soundbank;
 import java.awt.*;
 import java.util.Scanner;
 public class TestCuentaB {
 
-
-        public static int Menu() {
-            Scanner entrada = new Scanner(System.in);
-            int option;
-
-
-            System.out.println("[1] Saldo");
-            System.out.println("[2] Retirar");
-            System.out.println("[3] Depositar");
-            System.out.println("[0] Salir");
-            option = entrada.nextInt();
+    int dp;
+    public static int Menu() {
+        Scanner entrada = new Scanner(System.in);
+        int option;
 
 
-            return option;
 
-        }
+        System.out.println("[1] Saldo");
+        System.out.println("[2] Retirar");
+        System.out.println("[3] Depositar");
+        System.out.println("[0] Salir");
+        option = entrada.nextInt();
 
+
+        return option;
+
+    }
+
+
+    int deposit;
 
 
     public static void main(String[] args) {
-            int option = 0;
-            int valTemp;
+        int option = 0;
+
         System.out.println("Probando la clase Cuentabancaria ");
-Cuentabancaria Account = new Cuentabancaria();
+        Cuentabancaria Account = new Cuentabancaria();
+        Cuentabancaria Deposit = new Cuentabancaria();
 
 
-    Account.setBank_name("BBVA");
+
+
+
+        Account.setBank_name("BBVA");
         Account.setUsername("MirianBahena");
         Account.setAccount_number(12345678);
-       Account.setPassword("m24i/");
+        Account.setPassword("m24i/");
         Account.setBalance(2000.00);
 
-       // Account.toString(); //
+        // Account.toString(); //
 
-       do {
-         valTemp = Menu();
-       }while (option != 0);
-        System.out.println("La opcion seleccionada:  " + valTemp);
-        if (option == 1){
-            System.out.println("Tu saldo es: "  );
-            Account.getBalance();
-        }
+        do {
+            option = Menu();
+            if (option == 1) {
+                System.out.println("Tu saldo es: " + Account.getBalance());
 
-        if (option == 2){
-            System.out.println("Dijite la cantidad a retirar: " );
+            }
+            if (option == 2) {
+                System.out.println("Tu saldo en la cuenta es de: " + Account.getBalance());
 
-        }
+                System.out.println("Dijite la cantidad a depositar: ");
+
+                System.out.println("Tu saldo actual en tu cuenta es de: ");
+
+            }
+            if (option == 3) {
+                System.out.println("Tu saldo es de: " + Account.getBalance());
+
+                System.out.println("Dijite la cantidad a retirar: ");
+
+                System.out.println("Tu saldo actual es de: ");
 
 
-}
-}
+            }
+
+        }while (option != 0) ;
+            System.out.println("La opcion seleccionada:  " + option);
+
+
+            Account.toString();
+
+
+            }
+    }
+
+
 
