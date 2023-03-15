4e96397a3fd83cb407ead49ea64b30df8e80ff08
Mario vg <22670080@itiguala.edu.mx>
2023-03-14 19:50:48 -0600

    se creo la clase Test_SportShoe referente al examen

---
+++ src/Test_SportShoe.java
@@ -0,0 +1,17 @@
+public class Test_SportShoe {
+
+        public static void main(String[] args) {
+            SportShoe shoe = new SportShoe(
+                    "Mario",
+                    (short) 18,
+                    'M',
+                    "Lacoste y Adidas",
+                    (byte) 28,
+                    "verde y azul",
+                    2);
+
+            shoe.toString();
+        }
+    }
+
+
