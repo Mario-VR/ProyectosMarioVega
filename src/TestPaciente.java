58fab3235ede35a66d55cfb8bab6328287f8f8a9
Mario vg <22670080@itiguala.edu.mx>
2023-03-17 14:09:06 -0600

    se modifico la clase "TestPaciente"

--- src/TestPaciente.java
+++ src/TestPaciente.java
@@ -4,13 +4,13 @@ public class TestPaciente {
 
     public static void main(String[] args) {
         double imc = 0;
-        PacienteNutricion patient = new PacienteNutricion("Maria", "Morales", 20, 1.6, 65.500, 'F');
+        PacienteNutricion patient = new PacienteNutricion("Maria", "Morales", 20, 1.6, 65.500, 'F',"");
 
-       // imc=patient.imc(patient.getWeight(),patient.getHeight());//
+        imc=patient.imc(patient.getWeight(),patient.getHeight());
 
         System.out.println("Hola "+patient.getName()+", tu IMC es: "+imc);
-       // String compotition = patient.Compotition(imc);
-      //  System.out.println("Tu composicion corporal es: "+compotition);//
+       String compotition = patient.Compotition(imc);
+        System.out.println("Tu composicion corporal es: "+compotition);
 
     }
 }
